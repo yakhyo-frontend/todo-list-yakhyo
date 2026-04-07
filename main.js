c39f6d27c84d9492c7d2696a3d41317d4a624a6f
Yaxa <you@example.com>
2026-04-07 19:01:34 +0500

    delete function has done for to-do list

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
-const elList = document.querySelector('.todos');
-const elForm = document.querySelector('.write-todo');
-const elMessage = document.querySelector('.message');
+// const elList = document.querySelector('.todos');
+// const elForm = document.querySelector('.write-todo');
+// const elMessage = document.querySelector('.message');
+
+// let todos = [];
+
+// elForm.addEventListener('submit', (e) => {
+    
+//     e.preventDefault();
+
+//     const inputValue = elForm.querySelector('input').value.trim();
+
+//     if (!inputValue) return alert("Maydonni toldirish shart!");
+
+//     if (inputValue.length > 25) return alert("25 ta belgidan kam bo'lish kerak!");
+
+//     elMessage.textContent = '';
+
+//     const item = {
+//         inputValue: inputValue,
+//         date: '12, 12, 1212',
+//         completed: false
+//     };
+
+//     todos.push(item);
+
+//     showData(todos);
+
+//     elForm.reset();
+// });
+
+// function showData(todos) {
+//     elList.innerHTML = '';
+//     todos.forEach(item => {
+
+//         elList.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
+//         <span>${item.inputValue}</span>
+//         <i class="far fa-edit"></i>
+//         <i class="far fa-trash-alt delete" onclick="deleteTodo(${todos.indexOf(item)})"></i></li>`;
+
+//         console.log(item);
+//     });
+// }
+
+// function deleteTodo(index) {
+//     todos = todos.filter((item, i) => i !== index);
+//     showData(todos);
+// };
+
+
+elForm = document.querySelector('.write-todo');
+elList = document.querySelector('.todos');
+elMessage = document.querySelector('.message');
 
 let todos = [];
 
@@ -9,30 +59,39 @@ elForm.addEventListener('submit', (e) => {
 
     const inputValue = elForm.querySelector('input').value.trim();
 
-    if (!inputValue) return alert("Maydonni toldirish shart!");
+    if (!inputValue) return alert('Maydonni toldirish shart');
 
+    if (inputValue.length > 25) return alert('25-ta belgidan kam bolish kerak!');
 
-
+    elMessage.textContent = '';
+    
     const item = {
         inputValue: inputValue,
+        date: '12, 12, 1212',
         completed: false
-    };
+    }
 
     todos.push(item);
 
-    console.log(todos);
-    
     showData(todos);
+
+    elForm.reset();
 });
 
 function showData(todos) {
-    todos.forEach(item => {
+    elList.innerHTML = '';
 
+    todos.forEach((item) => {
         elList.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${item.inputValue}</span>
-        <i class="far fa-edit"></i>
-        <i class="far fa-trash-alt delete"></i></li>`;
+        <i class="far fa-edit"></i>         <i class="far fa-trash-alt delete" onclick="deleteTodo(${todos.indexOf(item)})"></i></li>`;
 
         console.log(item);
+
     });
+};
+
+function deleteTodo(index) {
+    todos = todos.filter((item, i) => i !== index);
+    showData(todos);
 }
